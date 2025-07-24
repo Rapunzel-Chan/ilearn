3ec36b4181307e749fe3c1f10a038035eb35e428
Rapunzel <osina_kv@mail.ru>
2025-07-24 22:39:38 +0300

    add lessons to course serializer

--- materials/serializers.py
+++ materials/serializers.py
@@ -12,13 +12,17 @@ class LessonSerializer(ModelSerializer):
 
 class CourseSerializer(ModelSerializer):
     count_lesson_of_the_same_course = SerializerMethodField()
+    lessons = LessonSerializer(many=True, read_only=True)
 
     def get_count_lesson_of_the_same_course(self, course):
         return course.lessons.count()
 
+    def get_lessons(self, course):
+        return [lesson.title for lesson in Lesson.objects.filter(course=course)]
+
     class Meta:
         model = Course
-        fields = ("title", "description", "count_lesson_of_the_same_course",)
+        fields = ("title", "description", "count_lesson_of_the_same_course", "lessons",)
 
 
 # class CourseDetailSerializer(ModelSerializer):
