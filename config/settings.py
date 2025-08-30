c1337de0b2eb8a2b838efc22318d4a89c89764fc
Rapunzel <osina_kv@mail.ru>
2025-08-30 20:00:57 +0300

    fixes

--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['176.108.245.219']
+ALLOWED_HOSTS = ['176.108.245.219', 'localhost', '127.0.0.1']
 
 # Application definition
 
