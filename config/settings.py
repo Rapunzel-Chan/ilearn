ad9e9c143b89cc5a341c9dc4ca06f8887a5990af
Rapunzel <osina_kv@mail.ru>
2025-09-05 21:49:22 +0300

    add settings for static

--- config/settings.py
+++ config/settings.py
@@ -148,7 +148,11 @@ USE_TZ = True
 # Static files
 STATIC_URL = "static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_ROOT = os.environ.get("STATIC_ROOT", BASE_DIR / "staticfiles")
+
+if os.getenv("DJANGO_PRODUCTION") == "1":
+    STATIC_ROOT = BASE_DIR / "staticfiles"
+else:
+    STATIC_ROOT = BASE_DIR / "static"
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
